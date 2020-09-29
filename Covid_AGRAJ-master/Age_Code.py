f42e6f92d2ce19c3ec85236e9fdeb9f6342d95ad
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-09-28 21:20:11 -0400

    Update Age_Code.py

--- Covid_AGRAJ-master/Age_Code.py
+++ Covid_AGRAJ-master/Age_Code.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import requests
-import io 
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline  
@@ -9,11 +7,7 @@ from sklearn.linear_model import (LinearRegression)
 
 
 def agedataframe(age):
-  url = ""
-  download = requests.get(url).content
-  df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-  
-  df = pd.read_csv("AgeCOVID-19DeathsData.csv") 
+  df = pd.read_csv("Covid_AGRAJ-master/AgeCOVID-19DeathsData.csv")
   df.drop(df[df['Age Group'] != user].index, inplace = True) # Dropping death counts data for all age groups except the one selected by user
   columns = df.columns
   first = columns.get_loc("COVID-19 Deaths")
@@ -28,14 +22,14 @@ def agedataframe(age):
      deaths = np.append(deaths, df.iloc[i, first]) # Need to access index[i][first]
      totaldeaths = np.append(totaldeaths, df.iloc[i, second]) #[i][second ]
      i += 1
-  deaths = deaths.reshape(-1,1)
-  totaldeaths = totaldeaths.reshape(-1,1)
+  deaths = deaths.reshape(-1,1) # reshaping the array so that it is 2-D
+  totaldeaths = totaldeaths.reshape(-1,1) # reshaping the array so that it is 2-D
   numericdeaths = np.array([i[0] for i in deaths]) # converting all strings to floats
   numerictotal = np.array([i[0] for i in totaldeaths]) # converting all strings to floats
   
-  # Implementing a linear regression model to predict COVID-19 deaths for a certain week
+  # Implementing a Polynomial Regression Model to predict COVID-19 deaths for a certain week
   model = make_pipeline(PolynomialFeatures(3), LinearRegression())
-  fittingmodel = model.fit(week, numericdeaths) #polynomial regression model
+  fittingmodel = model.fit(week, numericdeaths) #fitting data with a polynomial regression line 
   y_plot = model.predict(week[:, np.newaxis].reshape(-1,1))
   length = len(week)
   covid = int(model.predict(week[length-2].reshape(-1,1)))
