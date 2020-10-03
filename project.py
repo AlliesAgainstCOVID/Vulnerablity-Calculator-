ed716d0df2dc457e93627da141c5e53dbc27ab43
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-02 21:17:21 -0400

    Update project.py

--- project.py
+++ project.py
@@ -242,6 +242,11 @@ st.write("""
 if st.sidebar.button('Submit'):
         cg.render_gauge((int(output_df3)))
         #st.write(cg.render_gauge((int(output_df3))))
+        #AGE PERSONALIZED MESSAGE. 
+        #if age == "65-74 years" or age == "75-84 years" or age == "85 years and over":
+            #st.write("Statistical analysis on CDC Data shows that older age groups(65 years and Above) are more vulnerable to COVID-19, and these findings are consistent with sources such as the World Health Organization. To minimize mortality risk as much as possible, please make sure to limit interactions with other people and travelling to gatherings or outside environments like care facilities. If you have any underlying medical conditions, it’s also recommended to talk with your doctor and healthcare provider for working on a care plan that can help for emergencies during the pandemic. 
+            #Learn more here:
+            #https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/older-adults.html")
         st.write("Please continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
 #cg.render_gauge((int(output_df3)))
 
