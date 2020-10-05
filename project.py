c790f63972ba54d1f0db042023a2cf9f0514a722
AlliesAgainstCOVID <71732518+AlliesAgainstCOVID@users.noreply.github.com>
2020-10-05 16:59:07 -0400

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,8 @@ from PIL import Image
 import custom_gauge as cg
 
 image = Image.open('AI_19_image.jpg')
-st.image (image, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
+#st.image (image, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
+st.image (image, caption='A COVID-19 Mortality Risk Predictor', width=none, height=none)
 
 st.sidebar.header ("Select your inputs")
 #st.sidebar.subheader ("Select your inputs")
