bd2c905126893ed3a274293657369894693e3680
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-05 18:07:05 -0400

    Update project.py

--- project.py
+++ project.py
@@ -240,7 +240,7 @@ output_df3 = aggregate_calc()
 image = Image.open('AI_19_image.jpg')
 
 #st.image (image, caption='A COVID-19 Mortality Risk Predictor', width=None)
-im_resized = image.resize((image.width, 200))
+im_resized = image.resize((image.width, 300))
 st.image (im_resized, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
 
 st.write("""
