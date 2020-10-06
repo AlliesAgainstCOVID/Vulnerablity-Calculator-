25cc41bd585402117fd5942c954aa319304c44a8
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-06 19:23:34 -0400

    Update project.py

--- project.py
+++ project.py
@@ -19,7 +19,7 @@ local_css("style.css")
 st.beta_set_page_config(
 	page_title="Ex-stream-ly Cool App",
 	layout="wide",
-	initial_sidebar_state="expanded",
+	initial_sidebar_state="expanded"
 )
 
 #st.sidebar(fixed=True)
