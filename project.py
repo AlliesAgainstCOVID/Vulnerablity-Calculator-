abf9ef33d3a793e474cbaece8186ab94519f436c
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-07 17:26:29 -0400

    Update project.py

--- project.py
+++ project.py
@@ -275,10 +275,10 @@ if st.sidebar.button('Submit'):
                 #Learn more here: 
 	        #https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/older-adults.html """, unsafe_allow_html=True)
         else:
-        st.write("Please  continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
-        image2 = Image.open('DontWaitSelfIsolate_01.jpg')	 
-        #im_resized2 = image2.resize((image.width, 100)) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
-        st.image(image2, use_column_width=True) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
+		st.write("Please  continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
+		image2 = Image.open('DontWaitSelfIsolate_01.jpg')	 
+        	im_resized2 = image2.resize((image.width, 100)) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
+		st.image(im_resized, use_column_width=True) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
 #cg.render_gauge((int(output_df3)))
 
 fig = go.Figure(go.Indicator(
