37e84322ad48a7751b55042c39618b5ea7cbbfa1
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-07 18:42:24 -0400

    Update project.py

--- project.py
+++ project.py
@@ -262,8 +262,8 @@ imageLocation.image (imageone, caption='A COVID-19 Mortality Risk Predictor', wi
 
 if st.sidebar.button('Submit'):
         imagetwo = Image.open('2AI_19_image_01.jpg')
-        im_resizedtwo = imagetwo.resize((50,30)) #comment this if it doesn't work
-        imageLocation.image(im_resizedtwo, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
+        #im_resizedtwo = imagetwo.resize((50,30)) #comment this if it doesn't work
+        imageLocation.image(imagetwo, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
         # AH - This did not work st.subheader('Your Mortality Rate')
         #st.write(""" ### Mortality Rate: """)
         st.markdown(""" <h1 style='text-align: center; color: red;'>Your Mortality Rate</h1> """, unsafe_allow_html=True)
@@ -277,8 +277,8 @@ if st.sidebar.button('Submit'):
         else:
                 st.write("Please continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
         image2 = Image.open('DontWaitSelfIsolate_01.jpg')	 
-        im_resized2 = image2.resize((image2.width, 100))
-        st.image(im_resized2, use_column_width=True)
+        #im_resized2 = image2.resize((image2.width, 100))
+        st.image(image2, use_column_width=True)
 #cg.render_gauge((int(output_df3))
 
 fig = go.Figure(go.Indicator(
