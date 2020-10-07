2ada7f52546cae21c427bd0fc4af7b1d0460becd
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-07 14:32:45 -0400

    Update project.py

--- project.py
+++ project.py
@@ -254,8 +254,8 @@ output_df3 = aggregate_calc()
 imageLocation = st.empty()
 
 image = Image.open('AI_19_image.jpg')
-#st.image (image, caption='A COVID-19 Mortality Risk Predictor', width=None)
-#im_resized = image.resize((image.width, 200))
+st.image (image, caption='A COVID-19 Mortality Risk Predictor', width=None)
+im_resized = image.resize((image.width, 300))
 imageLocation.image (im_resized, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
 
 if st.sidebar.button('Submit'):
