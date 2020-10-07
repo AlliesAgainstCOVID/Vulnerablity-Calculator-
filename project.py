f6b92ea82c6124c2d08069cb5f3d2570efdcea54
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-07 16:32:12 -0400

    Update project.py

--- project.py
+++ project.py
@@ -277,7 +277,7 @@ if st.sidebar.button('Submit'):
         else:
                 st.write("Please continue to social distance and wear a mask in public. Keep at least 6 feet between you and others at all times. Do not be within contact for longer than 10 minutes. If you experience any symptoms, please isolate yourself and get tested as soon as possible.")
         image2 = Image.open('DontWaitSelfIsolate.JPG')	 
-        im_resized2 = image2.resize((image.width, 100)) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
+        #im_resized2 = image2.resize((image.width, 100)) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
         st.image(im_resized2, use_column_width=True) #AH - I am not sure why this is erroring out as "inconsistent use of tabs and spaces in indentation"
 #cg.render_gauge((int(output_df3)))
 
