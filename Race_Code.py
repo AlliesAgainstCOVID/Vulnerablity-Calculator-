4d4bb5765c443ce7a088ce7929d99d3fa86f06e0
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-15 08:41:34 -0400

    Update Race_Code.py

--- Race_Code.py
+++ Race_Code.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
