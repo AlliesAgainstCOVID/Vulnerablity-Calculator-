2274cdf7fcd2ff9416899a73184937f89bae866c
AdwaithHariharan <71349971+AdwaithHariharan@users.noreply.github.com>
2020-10-14 22:22:34 -0400

    Update project.py

--- project.py
+++ project.py
@@ -300,12 +300,8 @@ if st.sidebar.button('Submit'):
 	#Learn more here: 
 	#https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/cloth-face-cover-guidance.html """, unsafe_allow_html=True)
     else:
-	st.write(""" Please continue to social distance and wear a mask in public. If you experience any symptoms, please isolate yourself and get tested as soon as possible. Additionally, if you have any underlying medical conditions, it’s recommended to talk with your doctor for working on a care plan that can help for emergencies during the pandemic""")
-        #image2 = Image.open('DontWaitSelfIsolate_01.jpg')  AH- Commented out based on our 10/9 mtg discussion	 
-        #im_resized2 = image2.resize((image2.width, 100))
-        #st.image(image2, width=None)
-        #cg.render_gauge((int(output_df3))
-  else:
+	st.write("Please continue to social distance and wear a mask in public. If you experience any symptoms, please isolate yourself and get tested as soon as possible. Additionally, if you have any underlying medical conditions, it’s recommended to talk with your doctor for working on a care plan that can help for emergencies during the pandemic""")
+    else:
 	st.write("To get your results on your COVID-19 mortality risk, please fill out the fields in the sidebar on the left.")
 
 fig = go.Figure(go.Indicator(
