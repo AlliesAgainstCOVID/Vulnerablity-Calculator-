e88a2bca729b6bebfd538a64ef91a1ae2f86e7f9
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-15 08:30:10 -0400

    Update Race_Code.py

--- Race_Code.py
+++ Race_Code.py
@@ -7,7 +7,8 @@ from sklearn.metrics import r2_score
 #df = pd.read_csv("Covid_AGRAJ-master/RaceDeathCOVIDFinal.csv")
 df = pd.read_csv("RaceDeathCOVIDFinal.csv")
 
-white_population = 160626928
+#population size for each race
+white_population = 160626928 
 black_population = 31140331
 latinx_population = 41884672
 asian_population = 15221807
@@ -16,12 +17,12 @@ nhpi_population = 448851
 multiracial_population = 4158826
 
 def white(input):
- f = df["Deaths_White"].groupby(df["Deaths_White"].index // 56).sum()
- a = f.astype(np.int)
+ f = df["Deaths_White"].groupby(df["Deaths_White"].index // 56).sum() # grouping white deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/white_population
-     normalized.append(t*100)
+     t = a[i]/white_population # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
@@ -34,12 +35,12 @@ def white(input):
 
  
 def black(input):
- f = df["Deaths_Black"].groupby(df["Deaths_Black"].index // 56).sum()
- a = f.astype(np.int)
+ f = df["Deaths_Black"].groupby(df["Deaths_Black"].index // 56).sum() # grouping black deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/black_population
-     normalized.append(t*100)
+     t = a[i]/black_population # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
@@ -52,13 +53,13 @@ def black(input):
 
 
 def latinx(input):
- f = df["Deaths_LatinX"].groupby(df["Deaths_LatinX"].index // 56).sum()
- a = f.astype(np.int)
+ f = df["Deaths_LatinX"].groupby(df["Deaths_LatinX"].index // 56).sum() # grouping latinx deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/latinx_population
-     normalized.append(t*100)
- days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
+     t = a[i]/latinx_population  # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
+ days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1) # appending the area so that it contains the COVID-19 deaths
 
  model = LinearRegression()
  fittingmodel = model.fit(days, normalized) #fitting data with a Linear regression line 
@@ -70,12 +71,12 @@ def latinx(input):
 
  
 def asian(input):
- f = df["Deaths_Asian"].groupby(df["Deaths_Asian"].index // 56).sum()
- a = f.astype(np.int)
+ f = df["Deaths_Asian"].groupby(df["Deaths_Asian"].index // 56).sum() # grouping asian deaths
+ a = f.astype(np.int)  # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/asian_population
-     normalized.append(t*100)
+     t = a[i]/asian_population # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
@@ -87,13 +88,13 @@ def asian(input):
  return(probability)
 
  
-def aian(input):
- f = df["Deaths_AIAN"].groupby(df["Deaths_AIAN"].index // 56).sum()
- a = f.astype(np.int)
+def aian(input): 
+ f = df["Deaths_AIAN"].groupby(df["Deaths_AIAN"].index // 56).sum() # grouping American Indian/Alaskan Native deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/aian_population
-     normalized.append(t*100)
+     t = a[i]/aian_population # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
@@ -105,13 +106,13 @@ def aian(input):
  return(probability)
  
  
-def nhpi(input):
- f = df["Deaths_NHPI"].groupby(df["Deaths_NHPI"].index // 56).sum()
- a = f.astype(np.int)
+def nhpi(input): 
+ f = df["Deaths_NHPI"].groupby(df["Deaths_NHPI"].index // 56).sum() # grouping Native Hawaiian and Pacific Islander deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/nhpi_population
-     normalized.append(t*100)
+     t = a[i]/nhpi_population # normalizing the data by dividing with population size
+     normalized.append(t*100) # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
@@ -124,12 +125,12 @@ def nhpi(input):
  
  
 def multiracial(input):
- f = df["Deaths_Multiracial"].groupby(df["Deaths_Multiracial"].index // 56).sum()
- a = f.astype(np.int)
+ f = df["Deaths_Multiracial"].groupby(df["Deaths_Multiracial"].index // 56).sum() # grouping multiracial deaths
+ a = f.astype(np.int) # converting the type
  normalized = []
  for i in range (34):
-     t = a[i]/multiracial_population
-     normalized.append(t*100)
+     t = a[i]/multiracial_population # normalizing the data by dividing with population size
+     normalized.append(t*100)  # appending the area so that it contains the COVID-19 deaths
  days = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]).reshape(-1,1)
 
  model = LinearRegression()
