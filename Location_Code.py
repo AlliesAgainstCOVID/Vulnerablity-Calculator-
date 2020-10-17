6a2ae4648e2b5c5653eea735b68612783bfa3f93
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-16 22:04:44 -0400

    Update Location_Code.py

--- Location_Code.py
+++ Location_Code.py
@@ -6,7 +6,7 @@ def location_data(state):
  
     #ask_state = input("What state do you live in?\n")
     state_abbrev = {"Alabama" : "al", "Alaska" : "ak", "Arizona": "az", "Arkansas": "ar", "California": "ca","Colorado": "co","Connecticut": "ct", "Delaware": "de","Florida": "fl","Georgia":"ga","Hawaii": "hi","Idaho":"id","Illinois": "il", "Indiana": "in","Iowa":"ia", "Kansas": "ks","Kentucky": "ky","Louisiana": "la","Maine": "me", "Maryland": "md", "Massachusetts": "ma", "Michigan": "mi", "Minnesota": "mn", "Mississippi":"ms","Missouri": "mo", "Montana": "mt","Nebraska": "ne","Nevada": "nv","New Hampshire": "nh","New Jersey": "nj","New Mexico": "nm","New York": "ny","North Carolina": "nc","North Dakota": "nd","Ohio": "oh","Oklahoma":"ok","Oregon": "or","Pennsylvania": "pa" ,"Rhode Island": "ri","South Carolina": "sc","South Dakota": "sd", "Tennessee": "tn", "Texas": "tx","Utah": "ut","Vermont": "vt","Virginia": "va", "Washington": "wa","West Virginia": "wv", "Wisconsin": "wi", "Wyoming": "wy"}
-    state = state_abbrev.get(ask_state)
+    state = state_abbrev.get(state)
     
     csv_url = "https://covidtracking.com/api/v1/states/"+state+"/daily.csv"
  
