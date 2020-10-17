fae4e2beb4082c3d9e7f23b04a733ab7009b52ee
Shagun Khare <68030050+shagun-khare-917@users.noreply.github.com>
2020-10-16 21:57:18 -0400

    Update Location_Code.py

--- Location_Code.py
+++ Location_Code.py
@@ -6,10 +6,8 @@ def location_data(state):
  
     #ask_state = input("What state do you live in?\n")
     state_abbrev = {"Alabama" : "al", "Alaska" : "ak", "Arizona": "az", "Arkansas": "ar", "California": "ca","Colorado": "co","Connecticut": "ct", "Delaware": "de","Florida": "fl","Georgia":"ga","Hawaii": "hi","Idaho":"id","Illinois": "il", "Indiana": "in","Iowa":"ia", "Kansas": "ks","Kentucky": "ky","Louisiana": "la","Maine": "me", "Maryland": "md", "Massachusetts": "ma", "Michigan": "mi", "Minnesota": "mn", "Mississippi":"ms","Missouri": "mo", "Montana": "mt","Nebraska": "ne","Nevada": "nv","New Hampshire": "nh","New Jersey": "nj","New Mexico": "nm","New York": "ny","North Carolina": "nc","North Dakota": "nd","Ohio": "oh","Oklahoma":"ok","Oregon": "or","Pennsylvania": "pa" ,"Rhode Island": "ri","South Carolina": "sc","South Dakota": "sd", "Tennessee": "tn", "Texas": "tx","Utah": "ut","Vermont": "vt","Virginia": "va", "Washington": "wa","West Virginia": "wv", "Wisconsin": "wi", "Wyoming": "wy"}
- 
     state = state_abbrev.get(ask_state)
     
-    #state = input("Please enter the abbreviated form of your state in a lowercase format: \n")
     csv_url = "https://covidtracking.com/api/v1/states/"+state+"/daily.csv"
  
     totaldeaths = {"al": 29611, "ak": 2068, "az": 38945, "ar": 17293, "ca": 153910 , "co": 23447, "ct": 18372, "de": 5377, "fl": 121463, "ga": 47625, "hi": 6074, "id": 7719, "il": 65073, "in": 37404, "ia": 16424, "ks": 14288, "ky": 25005, "la": 27401, "me": 7952, "md": 31280, "ma": 38972, "mi": 58931, "mn": 25157, "ms": 18973, "mo": 34319, "mt": 5319, "ne": 8935, "nv": 14682, "nh": 7046, "nj": 55887, "nm": 10282, "ny": 66126, "nc": 42875, "nd": 3780 , "oh": 65811, "ok": 20335, "or" : 19284, "pa" :75308 , "ri": 5900, "sc": 29412, "sd": 4293, "tn": 41191, "tx": 117602, "ut": 10735, "vt": 3201 , "va": 39602, "wa": 31261, "wv": 9353, "wi": 29580, "wy": 2499} #Total Number Of Deaths For Each State
