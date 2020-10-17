d93340e66d89da85b403a6bb1d5bb547965cefb1
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-16 22:33:55 -0400

    Update project.py

--- project.py
+++ project.py
@@ -90,7 +90,6 @@ imageone = Image.open('AI_19_image_05.jpg')
 imageLocation.image (imageone, caption='A COVID-19 Mortality Risk Predictor', use_column_width=True)
 #st.write("To get your results on your COVID-19 mortality risk, please fill out the fields in the sidebar on the left.")
 
-# With results: no instructions, gauge, help text, banner image
 
 if st.sidebar.button('Submit'):
     image = Image.open('AI-19Logo.JPG')	
@@ -111,7 +110,7 @@ else:
     imageone = Image.open('AI_19_image_05.jpg')
     st.markdown("To get your results on your COVID-19 mortality risk, please select your inputs in the sidebar on the left.")
 st.write("")
-st.write("Disclaimer: Please note that the information in this web app is not medical advice but rather precautionary guidelines. Although it has involved content from medical professionals, it has not been endorsed by any doctor/healthcare provider. All information is for educational purposes only. Thank you!")
+st.markdown(""" <h3 style = 'color: red;'> Disclaimer: Please note that the information in this web app is not medical advice but rather precautionary guidelines. Although it has involved content from medical professionals, it has not been endorsed by any doctor/healthcare provider. All information is for educational purposes only. Thank you!</h3> """, unsafe_allow_html=True)
 st.write("Questions or Thoughts? Contact us at alliesagainstcovid@gmail.com")
 fig = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = finalprob,
