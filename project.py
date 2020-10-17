d5e0993081a4d8bca9da42a3769c4c6754c14693
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-16 22:11:41 -0400

    Update project.py

--- project.py
+++ project.py
@@ -131,21 +131,21 @@ if st.sidebar.button('Submit'):
     st.markdown(""" <h1 style='text-align: center; color: red;'>Your Mortality Rate</h1> """, unsafe_allow_html=True)
     cg.render_gauge((int(finalprob))) #output_df3
     #AGE PERSONALIZED MESSAGE. 
-    if age == "65-74 years" or age == "75-84 years" or age == "85 years and over":
-        st.markdown(""" Statistical analysis on CDC Data shows that older age groups 65 years and above are more vulnerable to COVID-19, and these findings are consistent with sources such as the World Health Organization. To minimize mortality risk as much as possible, please make sure to limit interactions with other people and travelling to gatherings or outside environments like care facilities. Learn more here: https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/older-adults.html """, unsafe_allow_html=True)
-    #Other statements
     if NPI2 == 0 and age != "Under 1 year" or age != "1-4 years":
         st.markdown(""" We strongly urge you to please start wearing masks when going out to public areas. According to the CDC, there are many studies that show masks can prevent the spread of respiratory droplets from the mouth, nose, and parts of the face. This is for your safety and others.
         Learn more here: 
         https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/cloth-face-cover-guidance.html """, unsafe_allow_html=True)
     else:
         st.write("Please continue to social distance(6ft. between you and others) and wear a mask in public. If you experience any symptoms, please isolate yourself and get tested as soon as possible. Additionally, if you have any underlying medical conditions, it’s recommended to talk with your doctor and healthcare provider for working on a care plan that can help for emergencies during the pandemic. ")
-# No results yet: add instructions, bigger image
+    if age == "65-74 years" or age == "75-84 years" or age == "85 years and over":
+        st.markdown(""" Statistical analysis on CDC Data shows that older age groups 65 years and above are more vulnerable to COVID-19, and these findings are consistent with sources such as the World Health Organization. To minimize mortality risk as much as possible, please make sure to limit interactions with other people and travelling to gatherings or outside environments like care facilities. Learn more here: https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/older-adults.html """, unsafe_allow_html=True)
+    #Other statements
+     No results yet: add instructions, bigger image
 else:
     imageone = Image.open('AI_19_image_05.jpg')
     st.markdown("To get your results on your COVID-19 mortality risk, please select your inputs in the sidebar on the left.")
-st.write("Questions or Thoughts? Contact us at alliesagainstcovid@gmail.com")
 st.write("Disclaimer: Please note that the information in this web app is not medical advice but rather precautionary guidelines. Although it has involved content from medical professionals, it has not been endorsed by any doctor/healthcare provider. All information is for educational purposes only. Thank you!")
+st.write("Questions or Thoughts? Contact us at alliesagainstcovid@gmail.com")
 fig = go.Figure(go.Indicator(
     mode = "number+gauge+delta", value = finalprob,
     gauge = {
