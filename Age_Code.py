6ce685a97c4da6b9d1e81b82e08b1718e6caadf2
kavyav5 <68877114+kavyav5@users.noreply.github.com>
2020-10-18 22:07:29 -0400

    Update Age_Code.py

--- Age_Code.py
+++ Age_Code.py
@@ -5,7 +5,6 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline        
 
 def agedataframe(age):
-  #AH- df = pd.read_csv("Covid_AGRAJ-master/AgeCOVID-19DeathsData.csv")
   df = pd.read_csv("AgeCOVID-19DeathsData.csv")
   df.drop(df[df['Age Group'] != age].index, inplace = True) # Dropping death counts data for all age groups except the one selected by user
   columns = df.columns
